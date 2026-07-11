c1e9815c16684ccb2f8c92d9a2f5227c12adacd9
Max <max1979rf@gmail.com>
2026-07-11 19:20:49 -0300

    teste

--- scratch/test.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient("https://hianqcduecxkwmfbnncf.supabase.co", "sb_publishable_A9EZwRwGAEhlVNXXY90cmQ_2E8TKmM0");
-
-async function run() {
-  const { data, error } = await supabase.from('schema_migrations').select('*');
-  console.log("Migrations:", { data, error });
-}
-run();
